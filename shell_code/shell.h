6bc9838d56802c848089f77c08952c17b2f6f9ce
leehomeecky <meecky4christ@gmail.com>
2023-05-12 21:55:27 -0700

    adding string functions

--- shell_code/shell.h
+++ shell_code/shell.h
@@ -11,5 +11,7 @@
 
 ssize_t mygetline(char **lineptr, size_t *n, FILE *stream);
 int _strlen(char *str);
+char *_strdup(char *str);
+int _strcmp(char *str1, char *str2);
 
 #endif
--- shell_code/str_functions.c
+++ shell_code/str_functions.c
@@ -10,7 +10,54 @@
 
 int _strlen(char *str)
 {
-	if (*str == '\0' || *str == NULL)
+	if (*str == '\0' || str == NULL)
 		return (0);
 	return (1 + _strlen(++str));
+}
+
+/**
+ * _strdup - a function that duplicate a string
+ *
+ * @str - pointer to string to be duplicated
+ *
+ * Return: pointer to duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	int len, i;
+	char *dup_str;
+
+	if (str == NULL || *str = '\0')
+		return (NULL);
+	len = _strlen(str);
+	dup_str = (char *)malloc(sizeof(char) * len + 1);
+	for (i = 0; str[i]; i++)
+	dup_str[i] = str[i];
+	dup_str[i] = '\0';
+	return (dup_str);
+}
+
+/**
+ * _strcmp - function to compare strings
+ *
+ * @str1: string to compare
+ * @str2: string to be compared to
+ *
+ * Return: 0 if te same else -1
+ */
+
+int _strcmp(char *str1, char *str2)
+{
+	int len1, len2, i;
+
+	len1 = _strlen(str1);
+	len2 = _strlen(str2);
+
+	if (len1 != len2)
+		return (-1);
+	for (i = 0; len1[i]; i++)
+	if (len1[i] != len2[i])
+		return (-1);
+	return (0);
 }
