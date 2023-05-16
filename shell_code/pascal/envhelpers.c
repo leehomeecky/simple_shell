99a693b508c04b98b3b27dd337ac0801668db75f
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-16 14:50:28 +0100

    envhelpers.c

--- shell_code/pascal/envhelpers.c
+++ shell_code/pascal/envhelpers.c
@@ -1,3 +1,23 @@
+
+
+char *_getenv(const char* name) 
+{
+
+    extern char** environ;
+    size_t namelen = strlen(name);
+    char** var;
+
+    for (var = environ; *var != NULL; ++var) {
+        if (strncmp(name, *var, namelen) == 0 && (*var)[namelen] == '=') {
+            return (*var + namelen + 1);
+        }
+    }
+
+    return (NULL);
+}
+
+
+
 #include "shellt.h"
 
 /**
