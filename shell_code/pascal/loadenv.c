a12a299389c26b036e89449fa340086d2d95bcb7
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-15 23:39:02 -0700

    unsetenv

--- shell_code/pascal/loadenv.c
+++ shell_code/pascal/loadenv.c
@@ -74,7 +74,7 @@ void _setenv(char *name, char *value, env_t *envdata)
 		if (_strcmp(var, name) == 0)
 		{
 		free(envdata->envVar[k]);
-/*	envdata->envVar[k] = copy_info(name_env, value);*/
+	/*free(environ[k]);*/
 	namelen = _strlen(name);
 	valuelen = _strlen(value);
 	varlen = namelen + valuelen + 2;
@@ -83,6 +83,12 @@ void _setenv(char *name, char *value, env_t *envdata)
 	_strcat(envdata->envVar[k], "=");
 	_strcat(envdata->envVar[k], value);
 	_strcat(envdata->envVar[k], "\0");
+	/*environ[k] = malloc(sizeof(char) * (varlen));*/
+	/*_strcpy(environ[k], name);*/
+	/*_strcat(environ[k], "=");*/
+	/*_strcat(environ[k], value);*/
+	/*_strcat(environ[k], "\0");*/
+	environ = envdata->envVar;
 			free(dupVar);
 			return;
 		}
@@ -90,20 +96,74 @@ void _setenv(char *name, char *value, env_t *envdata)
 	}
 
 	envdata->envVar = addenvMem(envdata->envVar, k, sizeof(char *) * (k + 2));
-//	envdata->envVar[k] = copy_info(name, value);
+/*	environ = addenvMem(environ, k, sizeof(char *) * (k + 2));*/
 	namelen = _strlen(name);
 	valuelen = _strlen(value);
 	varlen = namelen + valuelen + 2;
 	envdata->envVar[k] = malloc(sizeof(char) * (varlen));
+/*	environ[k] = malloc(sizeof(char) * (varlen));*/
 	_strcpy(envdata->envVar[k], name);
 	_strcat(envdata->envVar[k], "=");
 	_strcat(envdata->envVar[k], value);
 	_strcat(envdata->envVar[k], "\0");
 	envdata->envVar[k + 1] = NULL;
+	/*_strcpy(environ[k], name);*/
+	/*_strcat(environ[k], "=");*/
+	/*_strcat(environ[k], value);*/
+	/*_strcat(environ[k], "\0");*/
+	/*environ[k + 1] = NULL;*/
 environ = envdata->envVar;
 }
 
+/**
+ * _unset - =======
+ * @envdata: =====
+ * @name: ======
+ * Return: ========
+ */
+int _unset(char *name, env_t *envdata)
+{
+	char **_environ;
+	char *dupvar, *nametounset;
+	int m, i, j;
 
+	if (name == NULL)
+	{
+		//get_error(datash, -1);
+		return (1);
+	}
+	m = -1;
+	for (i = 0; envdata->envVar[i]; i++)
+	{
+		dupvar = _strdup(envdata->envVar[i]);
+		nametounset = _strtok3(dupvar, "=");
+		if (_strcmp(nametounset, name) == 0)
+		{
+			m = i;
+		}
+		free(dupvar);
+	}
+	if (m == -1)
+	{
+	/*	get_error(datash, -1);*/
+		return (1);
+	}
+	_environ = malloc(sizeof(char *) * (i));
+	for (i = j = 0; envdata->envVar[i]; i++)
+	{
+		if (i != m)
+		{
+			_environ[j] = envdata->envVar[i];
+			j++;
+		}
+	}
+	_environ[j] = NULL;
+	free(envdata->envVar[m]);
+	free(envdata->envVar);
+	envdata->envVar = _environ;
+	environ = _environ;
+	return (1);
+}
 
 
 
@@ -146,8 +206,13 @@ if (command[len - 1] == '\n')
         token = _strtok3(NULL, " ");
     }
 
+    if (_strcmp(args[0], "setenv") == 0)
+	    _setenv(args[1], args[2], ev);
+    else if(_strcmp(args[0], "unsetenv") == 0)
+	    _unset(args[1], ev);
+    else
+	    return (1); /* error must be here*/
 
-_setenv(args[1], args[2], ev);
 
 for (i = 0; ev->envVar[i]; i++)                           printf("%s \n",  ev->envVar[i]);
 for (i = 0; environ[i]; i++)
