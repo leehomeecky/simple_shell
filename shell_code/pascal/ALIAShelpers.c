d8a76c735f9dcaafa88c636a895f6f569d803add
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-18 14:43:10 -0700

    alias

--- shell_code/pascal/ALIAShelpers.c
+++ shell_code/pascal/ALIAShelpers.c
@@ -1,10 +1,10 @@
 #include "shellt.h"
 
 /**
-  *removeExtraSpaces - =========
-  *@str: ==========
-  *Return: ==========
-  */
+ * removeExtraSpaces - =========
+ * @str: =====
+ * Return: ==========
+ */
 
 void removeExtraSpaces(char *str) 
 {
@@ -14,7 +14,8 @@ void removeExtraSpaces(char *str)
     /*Remove leading spaces*/
 	while (len > 0 && str[0] == ' ')
 	{
-        for (i = 0; i < len; i++) {
+        for (i = 0; i < len; i++)
+	{
             str[i] = str[i + 1];
         }
         len--;
@@ -23,8 +24,10 @@ void removeExtraSpaces(char *str)
     /*Remove extra spaces*/
     for (i = 0; i < len - 1; i++) 
     {
-        if (str[i] == ' ' && str[i + 1] == ' ') {
-            for (j = i; j < len; j++) {
+        if (str[i] == ' ' && str[i + 1] == ' ')
+	{
+            for (j = i; j < len; j++) 
+	    {
                 str[j] = str[j + 1];
             }
             len--;
@@ -46,65 +49,49 @@ void removeExtraSpaces(char *str)
   *Return: void
   */
 void load_aliases() 
-
 {
-
-	char buffer[BUFFER_SIZE];	ssize_t bytesRead;
-
+	char buffer[BUFFER_SIZE];
+	ssize_t bytesRead;
 	char line[BUFFER_SIZE];
-
 	int i, fileDescriptor, lineLength = 0;
 
 /*    fileDescriptor = open(ALIAS_FILE, O_RDONLY);*/
 
 	fileDescriptor = open(ALIAS_FILE, O_CREAT | O_RDWR, 0666);
+	if (fileDescriptor == -1) 
+	{
+perror("Error opening aliases file");
+return;
+}
 
-    if (fileDescriptor == -1) 
-
-    {
-
-        perror("Error opening aliases file");
-
-        return;
-
-    }
-
-    while ((bytesRead = read(fileDescriptor, buffer, BUFFER_SIZE)) > 0) {
-
-        for (i = 0; i < bytesRead; i++) {
-
-            if (buffer[i] == '\n') {
-
-                line[lineLength] = '\0'; 
+    while ((bytesRead = read(fileDescriptor, buffer, BUFFER_SIZE)) > 0)
+{
+        for (i = 0; i < bytesRead; i++)
+	{
+            if (buffer[i] == '\n') 
+	    {
+	line[lineLength] = '\0'; 
 
 		/* Null-terminate the line*/
-
-                _writef("%s\n", line);
-
-                lineLength = 0;
-
-            } else {
-
-                line[lineLength] = buffer[i];
-
-                lineLength++;
-
-            }
-
-        }
-
-    }
-
+	_writef("%s\n", line);
+	lineLength = 0;
+	    }
+	    else 
+	    {
+	line[lineLength] = buffer[i];
+	lineLength++;
+	    }
+	}
+}
 close(fileDescriptor);
-
 }
 
 /**
- *save_alias - ========
-  *@name: ========
-  *@value: ==========
-  *Return: ============
-  */
+ * save_alias - ========
+ * @name: ========
+ * @value: ==========
+ * Return: ============
+ */
 /*Function to save an alias to file*/
 void save_alias(char *name, char *value) 
 {
@@ -113,7 +100,8 @@ void save_alias(char *name, char *value)
     
     /*Open the file in append mode*/
     fileDescriptor = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-    if (fileDescriptor < 0) {
+    if (fileDescriptor < 0)
+    {
         _writef("Error opening the file.\n");
         return;
     }
