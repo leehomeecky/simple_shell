844940154cee8359273913d6f763ed867b182261
leehomeecky <55804777+leehomeecky@users.noreply.github.com>
2023-05-19 08:38:36 +0100

    Update shell.c

--- shell_code/shell.c
+++ shell_code/shell.c
@@ -56,9 +56,11 @@ int main(int argc, const char **argv)
 			if (shell_logic(argv, cmd_arr[i]))
 				_puts("exit");
 		}
+		if (cmd_arr)
 		free(cmd_arr);
 		_prompt();
 	}
+	if (line)
 	free(line);
 	return (0);
 }
