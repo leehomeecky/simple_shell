4c04d61e6f1f27fca575e5274a7091021ce56f0e
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-19 05:24:36 +0100

    ALIAShelpers.c

--- shell_code/pascal/ALIAShelpers.c
+++ shell_code/pascal/ALIAShelpers.c
@@ -117,3 +117,79 @@ void save_alias(char *name, char *value)
     close(fileDescriptor);
 
 }
+
+
+
+
+/*Function to retrieve and display an alias*/
+void retrieve_alias(char *name)
+{
+	char alias_name[MAX_ALIAS_NAME];
+	char alias_value[MAX_ALIAS_VALUE];
+	char *equalsSign, *quoteEnd, *quoteStart, *alias;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytesRead;
+	char line[BUFFER_SIZE];
+	int seen, i, fileDescriptor, lineLength = 0;
+/*	fileDescriptor = open(ALIAS_FILE,O_CREAT | O_RDONLY, 0666);*/
+	fileDescriptor = open(ALIAS_FILE, O_CREAT | O_RDWR, 0666);
+	if (fileDescriptor == -1)
+	{
+		perror("Error opening aliases file");
+		return;
+	}
+
+while ((bytesRead = read(fileDescriptor, buffer, BUFFER_SIZE)) > 0)
+{
+	for (i = 0; i < bytesRead; i++) 
+	{ 
+	if (buffer[i] == '\n') {
+		line[lineLength] = '\0'; 
+		/* Null-terminate the line*/ 
+		if (_strstr(line, "alias") != NULL) 
+		{
+        equalsSign = _strchr(line, '=');
+            if (equalsSign != NULL) {
+	quoteStart = _strchr(line, '\'');
+        quoteEnd = _strrchr(line, '\'');
+
+                if (quoteStart != NULL && quoteEnd != NULL && quoteEnd > quoteStart) {
+
+_strncpy(alias_name, line + 6, equalsSign - (line + 6));
+	alias_name[equalsSign - (line + 6)] = '\0';
+                    _strncpy(alias_value, quoteStart + 1, quoteEnd - quoteStart - 1);
+		    alias_value[quoteEnd - quoteStart - 1] = '\0';
+
+        /* Perform the comparison and write to stdout*/
+                    if (_strcmp(alias_name, name) == 0) {
+/* formatted output with puts*/
+alias = malloc(sizeof(char) * (_strlen(alias_name) +  4 + _strlen(alias_value))); 
+_strcat(alias, alias_name);     
+_strcat(alias, "=");
+_strcat(alias, "'");
+_strcat(alias, alias_value);
+_strcat(alias, "'");
+_strcat(alias, "\0");
+puts(alias);
+free(alias);
+seen = 1;
+                    }
+
+                }
+            }
+        }
+lineLength = 0;
+	} else {
+		line[lineLength] = buffer[i];
+		lineLength++;
+	}
+	}
+}
+
+if (seen != 1)
+{
+	_writef("bash: alias: %s: not foubd\n", name);
+}
+/*============================*/
+close(fileDescriptor);
+}
