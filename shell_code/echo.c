b6ca3696782c8c3fe23e1b38d12b735d987aae47
leehomeecky <meecky4christ@gmail.com>
2023-05-21 13:48:44 -0700

    perfecting variable replacement

--- shell_code/echo.c
+++ shell_code/echo.c
@@ -25,22 +25,27 @@ void remove_quotes(char *str, char quote)
 
 
 /**
- * count_quotes_int - function to count quote in string
+ * check_quote - function to if its a valid quote
  *
- * @str: string to check
+ * @str: caracter to check
  * @quote: type of quote to check
+ * @pos: position of quote in string
  *
- * Return: numbers of quote found
+ * Return: 1 if valid quote || zero if not
  */
 
-int count_quotes_int(char *str, char quote)
+int check_quote(char *str, char quote, int pos)
 {
-	int i, count = 0;
-
-	for (i = 0; str[i]; i++)
-		if (str[i] == quote && str[(i - 1)] != '\\')
-			count++;
-	return (count);
+	if (str[pos] == quote)
+	{
+		if (pos > 0)
+		{
+			if (str[(pos - 1)] == '\\')
+				return (0);
+		}
+		return (1);
+	}
+	return (0);
 }
 
 /**
@@ -90,25 +95,14 @@ arrQut *count_quotes_arr(char **cmd_arr)
 
 char *var_handler(char *str)
 {
-	int i, j = 0, s_q = 0, d_q = 0, *p;
-	char *space;
+	int i, j = 0;
+	char *space = (char *)malloc(BUFFER_SIZE);
 
-	space = (char *)malloc(BUFFER_SIZE);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == '\'')
-			p = &s_q;
-		else if (str[i] == '"')
-			p = &d_q;
-		if (str[i] == '\'' || str[i] == '"')
-			if (i > 0)
-			{
-			if (str[(i - 1)] != '\\')
-				(*p)++;
-			}
-			else
-				(*p)++;
-		if (str[i] == '$' && s_q % 2 == 0 &&  d_q % 2 == 0)
+		s_qu += check_quote(str, '\'', i);
+		d_qu += check_quote(str, '"', i);
+		if (str[i] == '$' && s_qu % 2 == 0 &&  d_qu % 2 == 0)
 		{
 			if (str[(i + 1)] == '$')
 			{
@@ -124,10 +118,8 @@ char *var_handler(char *str)
 			}
 			else if ((str[(i + 1)] >= 'a' && str[(i + 1)] <= 'z') ||
 			(str[(i + 1)] >= 'A' && str[(i + 1)] <= 'Z') || str[(i + 1)] >= '_')
-			{
-				j += write_str(&(space[j]), _getenv(&(str[(i + 1)])));
+			if (write_str(&(space[j]), _getenv(&(str[(i + 1)]))) >= 0)
 				return (space);
-			}
 		}
 		space[j++] = str[i];
 	}
@@ -148,6 +140,8 @@ void echo_func(char **cmd_arr, const char *prog_name)
 	int i;
 	arrQut *arr_qut;
 
+	s_qu = 0;
+	d_qu = 0;
 	arr_qut = count_quotes_arr(cmd_arr);
 	for (i = 1; cmd_arr[i]; i++)
 	cmd_arr[i] = var_handler(cmd_arr[i]);
@@ -157,6 +151,6 @@ void echo_func(char **cmd_arr, const char *prog_name)
 	free(arr_qut);
 	execve_func(cmd_arr, prog_name);
 	for (i = 1; cmd_arr[i]; i++)
-		free (cmd_arr[i]);
+		free(cmd_arr[i]);
 
 }
